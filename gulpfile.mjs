351f22a995310090a5546395cfac7de20ece8a8b
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2024-10-30 00:45:30 +0100

    chore: improve gulp workflows
    - add step to bundle JS using Rollup
    - add step to build CSS

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,38 +1,71 @@
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 
+import rollup from "@rollup/stream";
 import gulp from "gulp";
 import gulpPug from "gulp-pug";
 import { rimraf } from "rimraf";
+import source from "vinyl-source-stream";
+import buffer from "vinyl-buffer";
 
-const SOURCE_DIRECTORY = "src";
+const BUILD_DIRECTORY = ".cache";
 const PACKAGE_DIRECTORY = "dist";
+
+// copy assets to build directory
+function copyAssets() {
+  return gulp
+    .src("src/assets/**/*", { base: "src/", encoding: false })
+    .pipe(gulp.dest(BUILD_DIRECTORY, { mode: 0o644, dirMode: 0o755 }));
+}
+
+function copyTemplate() {
+  return gulp.src("src/**/*.pug", { base: "src/" }).pipe(gulp.dest(BUILD_DIRECTORY, { mode: 0o644, dirMode: 0o755 }));
+}
+
+const copyStaticAssets = gulp.parallel(copyAssets, copyTemplate);
+
+function buildCss() {
+  return gulp.src("src/**/*.css", { base: "src/" }).pipe(gulp.dest(BUILD_DIRECTORY, { mode: 0o644, dirMode: 0o755 }));
+}
+
+function buildJs() {
+  return rollup({ input: "./src/js/index.js", output: { format: "iife" } })
+    .pipe(source("js/index.js"))
+    .pipe(buffer())
+    .pipe(gulp.dest(BUILD_DIRECTORY));
+}
+
+const buildSource = gulp.parallel(buildCss, buildJs);
 
 // builds index.html from index.pug etc.
 function buildHtml() {
   return gulp
-    .src(`${SOURCE_DIRECTORY}/index.pug`)
+    .src(`${BUILD_DIRECTORY}/index.pug`)
     .pipe(gulpPug({ data: require("./package.json"), pretty: false })) // true adds extra spaces :-(
     .pipe(gulp.dest(PACKAGE_DIRECTORY, { mode: 0o644 }));
 }
 
+const cleanBuildFolder = () => rimraf(BUILD_DIRECTORY);
+
 const cleanPackageFolder = () => rimraf(PACKAGE_DIRECTORY);
 
 // npm run clean / npx gulp clean: clean '.cache' and 'dist' folder
-export const clean = gulp.series(cleanPackageFolder);
+export const clean = gulp.parallel(cleanBuildFolder, cleanPackageFolder);
 
 // npm run build / npx gulp build: build index.html and assets
-export const build = gulp.series(buildHtml);
+export const build = gulp.series(copyStaticAssets, buildSource, buildHtml);
 
 // npm run dist / npx gulp dist: copy just needed files to `dist` directory
 export const dist = gulp.series(clean, build);
 
 // watch files for changes and trigger rebuild tasks
 async function watchFiles() {
-  gulp.watch("src/**/*.{pug,js,css}", buildHtml);
+  gulp.watch("src/assets/*", gulp.series(copyAssets, buildHtml));
+  gulp.watch("src/**/*.{js,css}", gulp.series(buildSource, buildHtml));
+  gulp.watch("src/**/*.pug", gulp.series(copyTemplate, buildHtml));
 }
 
 // npm run watch / npx gulp watch: continuously update index.html from deps
-export const watch = gulp.series(buildHtml, watchFiles);
+export const watch = gulp.series(cleanBuildFolder, build, watchFiles);
 
 export default build;
